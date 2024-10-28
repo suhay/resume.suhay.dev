ad61fb9bc34a817ec900446a0d2fbcdfc2e5e8cf
Matt Suhay <matthew.suhay@gmail.com>
2024-10-28 14:38:26 -0500

    Adding zeroin stuff

---
+++ src/components/experience/zeroin.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ZeroIn = () => {
+  return (
+    <div className='job row'>
+      <h3 className='row'>Founder · Chief Technology Officer</h3>
+      <h4 className='row'>ZeroIn — Remote — Feb 2024 - Present</h4>
+      <ul>
+        <li>
+          Built and led a team of 6 individuals across multiple departments to develop a platform
+          that helps people find the best products for their sustainability needs.
+        </li>
+        <li>
+          Constructed a sustainable marketplace and climate action habit builder from the ground up
+          releasing a web app in the summer of 2024 and a mobile app soon after.
+        </li>
+        <li>
+          Developed a custom recommendation engine that uses machine learning to suggest products
+          based on a user's preferences and sustainability goals.
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default ZeroIn
